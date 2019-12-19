b5490277d3611f7548e5871a77058323608bc572
“7248510” <56181475+7248510@users.noreply.github.com>
2019-12-18 19:41:28 -0500

    Added file saving functionality

--- file.h
+++ file.h
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
 void file()
 {
 	using std::string;
 	using std::cout;
 	using std::cin;
+	using std::ofstream;
 	string y;
 	fileInfo file;
 	file.txt = ".txt";
@@ -13,4 +14,9 @@ void file()
 	cin >> file.fileName;
 	y = file.fileName + file.txt;
 	cout << "Your text file is called " << y << ".\n";
+	ofstream fname;
+	fname.open(y);
+	//fname << y; Local variables write, get the other parameters.
+	//Start writing details/current obstacle
+	fname.close();
 }
